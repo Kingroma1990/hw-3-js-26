c1716f9527bdffafa7af88f48254f8275a00de55
Roma <king_roma@ukr.net>
2019-12-10 15:21:40 +0200

    m3

---
+++ js/hw.3.3.js
@@ -0,0 +1,50 @@
+"use strict";
+const findBestEmployee = function(employees) {
+  let max = 0;
+  let name;
+  const key = Object.keys(employees);
+
+  for (const keys of key) {
+    if (max < employees[keys]) {
+      max = employees[keys];
+      name = keys;
+      // Object.entries(name);
+      // Object.entries(max);
+      // console.log(name);
+      // console.log(max);
+    }
+  }
+  return {
+    name,
+    max,
+  };
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(
+  findBestEmployee({
+    ann: 29,
+    david: 35,
+    helen: 1,
+    lorence: 99
+  })
+); // lorence
+
+console.log(
+  findBestEmployee({
+    poly: 12,
+    mango: 17,
+    ajax: 4
+  })
+); // mango
+
+console.log(
+  findBestEmployee({
+    lux: 147,
+    david: 21,
+    kiwi: 19,
+    chelsy: 38
+  })
+); // lux
